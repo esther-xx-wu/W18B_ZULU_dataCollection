46b1f04d360e9f0e25ab06d5387be7bffd7c6903
Esther Wu <z5416636@ad.unsw.edu.au>
2025-03-14 17:20:47 +1100

    Create sample test files and redo pytest and mypy steps in CI pipeline

---
+++ tests/test_sample.py
@@ -0,0 +1,14 @@
+# TODO: delete this file once tests are set up
+
+from ..src.sample import add_one # Edit subdirectory to get desired file
+
+# Prefix all test functions with "test_"
+
+def test_add_one():
+    assert add_one(1) == 2
+
+def test_always_passes():
+    assert True
+
+def test_always_fails():
+    assert False
