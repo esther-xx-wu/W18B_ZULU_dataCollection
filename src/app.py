5dfb6151fa8e2b6f3e5df5cfe7daf4a08db42226
Koushik Anand Pirabu <z5421830@ad.unsw.edu.au>
2025-04-01 19:21:11 +1100

    finish implementation of /traffic/single/v1

--- src/app.py
+++ src/app.py
@@ -32,30 +32,6 @@ def handle_single_suburb_traffic():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-@app.route('/traffic/multiple/v1', methods=['GET'])
-def handle_single_suburb_traffic():
-    try:
-        suburb = request.get('suburb')  
-        if not suburb:
-            return jsonify({"error": "Suburb is required"}), 400
-        elif not request.args.get('numDays'):
-            return jsonify({"error": "Number of days is required"}), 400
-        elif not (request.args.get('numDays')).isdigit():
-            return jsonify({"error": "Number of days must be a valid integer!"}), 400
-        
-        numDays = int(request.args.get('numDays'))
-
-        csv_data = fetch_traffic_data(suburb, numDays)
-        # s3_url = upload_to_s3(csv_data, suburb)
-
-        resp = make_response(csv_data)
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Content-Type'] = 'text/csv'
-        return resp
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
