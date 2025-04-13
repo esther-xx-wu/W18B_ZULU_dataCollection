44d7d1a08f005f332d3918e106ecefdbfbd961a3
Koushik Anand Pirabu <z5421830@ad.unsw.edu.au>
2025-04-13 19:21:37 +1000

    implement POST route for traffic/multiple/v1

--- src/app.py
+++ src/app.py
@@ -7,14 +7,64 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello from Flask on AWS Lambda!")
+    return jsonify(message="Traffic Collection Microservice is active!")
 
 @app.route('/traffic/single/v1', methods=['GET'])
 def handle_single_suburb_traffic():
+    """
+    Collects and retrieves traffic data for given suburbs for the number of days provided.
+
+    Args:
+        suburbs (suburb): Name of suburb.
+        numDays (int): Number of days to retrieve data for.
+
+    Returns:
+        Response: JSON data.
+    """
     try:
         suburb = request.args.get('suburb')
         numDays = request.args.get('numDays')
-        traffic_data = fetch_traffic_data(suburb, numDays)
+        traffic_data = fetch_traffic_data(suburb, numDays, 'single', 'json')
+        traffic_data_json = json.loads(traffic_data)
+        if "error" in traffic_data_json:
+            return jsonify({"error": traffic_data_json["error"]}), traffic_data_json["code"]
+
+        resp = make_response(traffic_data)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/traffic/multiple/v1', methods=['POST'])
+def handle_multiple_suburb_traffic():
+    """
+    Collects and retrieves traffic data for given suburbs for the number of days provided.
+
+    Args:
+        suburbs (list[str]): List of suburb names (in request body).
+        numDays (int): Number of days to retrieve data for.
+        format (str): Output format. Options: 'json', 'csv'.
+
+    Returns:
+        Response: JSON or temp download link.
+    """
+    try:
+        suburbs = request.get_json().get('suburbs', [])
+        numDays = request.args.get('numDays')
+        format = request.args.get('format')
+
+        if format != 'csv' and format != 'json':
+            return jsonify({"error": "Invalid or unsupported format provided"}), 400
+        
+        if not isinstance(suburbs, list):
+            return jsonify({'error': 'Expected a list of suburbs'}), 400
+        
+        if len(suburbs) > 10:
+            return jsonify({'error': 'More than 10 suburbs provided'}), 400
+
+        traffic_data = fetch_traffic_data(suburbs, numDays, 'multiple', format)
         traffic_data_json = json.loads(traffic_data)
         if "error" in traffic_data_json:
             return jsonify({"error": traffic_data_json["error"]}), traffic_data_json["code"]
